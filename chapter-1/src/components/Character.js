eb5d567b788f1baa9a9d1b0bef46d6cf015641e6
ChisKien <chiskien03214@gmail.com>
2022-09-05 09:07:43 +0700

    Character.js components

--- chapter-1/src/components/Character.js
+++ chapter-1/src/components/Character.js
@@ -1,9 +1,9 @@
 export function Character(character) {
-
     return (
         <div>
             <h3>{character.name}</h3>
-            <img src={character.image} alt={character.name}/>
+            <img src={character.image} alt={character.name} width="300"/>
+            <p>{`Origin: ${character.origin} && ${character.origin.name}`}</p>
         </div>
-    )
+    );
 }
