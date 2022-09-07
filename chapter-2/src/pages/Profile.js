cc79444a24cedf24afbd6683bf45ed2155d4bb38
ChisKien <chiskien03214@gmail.com>
2022-09-07 09:25:00 +0700

    Fix Bug

--- chapter-2/src/pages/Profile.js
+++ chapter-2/src/pages/Profile.js
@@ -1,33 +1,44 @@
 import {useEffect, useState} from "react";
+import "./Profile.css";
+import {Link} from "../components/Header/Link";
 
 export function Profile({username}) {
     const [loading, setLoading] = useState(false);
     const [profile, setProfile] = useState({});
 
     useEffect(() => {
-            async function fetchData() {
-                const response = await fetch(`https://api.github.com/users/${username}`);
-                const data = await response.json();
-
-                if (data) {
-                    setProfile(data);
-                    setLoading(true);
-                }
+        async function fetchData() {
+            const response = await fetch(`https://api.github.com/users/${username}`);
+            const result = await response.json();
+            if (result) {
+                setProfile(result);
+                setLoading(false);
             }
+        }
 
-            fetchData().then(r => console.log(r));
-        }, [username]
-    )
+        fetchData();
+    }, [username]);
 
-    return (
-        <div>
+    return (<div className={`Profile-container`}>
+        {loading ? (<span>Loading.....</span>) : (<div>
             <h2>About me</h2>
-            {loading ? (<span>Loading.....</span>) : (
-                <ul>
-
-                </ul>
-            )}
-        </div>
-    )
-
+            <img src={profile.avatar_url} alt={profile.name} className="Profile-avatar"/>
+            <ul>
+                <li>
+                    <span>html_url: </span>
+                    <Link url={profile.html_url} title={profile.html_url}/>
+                </li>
+                <li>
+                    <span>repos_url: </span>
+                    <Link url={profile.repos_url} title={profile.repos_url}/>
+                </li>
+                <li>
+                    <span>Name: {profile.name}</span>
+                </li>
+                <li>
+                    <span>Location: {profile.location}</span>
+                </li>
+            </ul>
+        </div>)}
+    </div>)
 }
